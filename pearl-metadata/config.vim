555a7f7b5864ae3bd14d177f7d8d2257f9f57e68
Filippo Squillace <squillace.filippo@gmail.com>
2016-03-31 21:49:16 +0100

    First commit!

---
+++ pearl-metadata/config.vim
@@ -0,0 +1,10 @@
+
+set rtp+=$PEARL_HOME/packages/powerline/module/powerline/bindings/vim/
+
+" Always show statusline
+set laststatus=2
+
+" Use 256 colours (Use this setting only if your terminal supports 256 colours)
+set t_Co=256
+
+
---
+++ pearl-metadata/install.sh
@@ -0,0 +1,35 @@
+
+function post_install(){
+    mkdir -p ${HOME}/.fonts
+    mkdir -p ${HOME}/.fonts.conf.d/
+    ln -s ${PEARL_HOME}/packages/powerline/module/font/PowerlineSymbols.otf ${HOME}/.fonts
+    fc-cache -vf ${HOME}/.fonts
+
+    cp ${PEARL_HOME}/packages/powerline/module/font/10-powerline-symbols.conf ${HOME}/.fonts.conf.d/
+
+    info "Vim binding applied"
+    if ask "Do you want Powerline binding also for Bash?" "N"
+
+    then
+        apply "source ${PEARL_HOME}/packages/powerline/module/powerline/bindings/bash/powerline.sh" "${HOME}/.bashrc" false
+    fi
+    if ask "Do you want Powerline binding also for Tmux?" "N"
+    then
+        apply "source ${PEARL_HOME}/packages/powerline/module/powerline/bindings/tmux/powerline.conf" "${HOME}/.tmux.conf" false
+    fi
+
+    return 0
+}
+
+function pre_remove(){
+    rm -f ${HOME}/.fonts/PowerlineSymbols.otf
+    fc-cache -vf ${HOME}/.fonts
+
+    local powerline_conf_file="${HOME}/.fonts.conf.d/10-powerline-symbols.conf"
+    [ -f "$powerline_conf_file" ] && rm -f $powerline_conf_file
+
+    unapply "source ${PEARL_HOME}/packages/powerline/module/powerline/bindings/bash/powerline.sh" "${HOME}/.bashrc"
+    unapply "source ${PEARL_HOME}/packages/powerline/module/powerline/bindings/tmux/powerline.conf" "${HOME}/.tmux.conf"
+
+    return 0
+}
