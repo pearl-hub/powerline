dd76e7a57ff47fdfd91fca9cb74aedba434645c4
Filippo Squillace <squillace.filippo@gmail.com>
2016-04-02 01:32:52 +0100

    First commit!

--- pearl-metadata/install.sh
+++ pearl-metadata/install.sh
@@ -2,20 +2,20 @@
 function post_install(){
     mkdir -p ${HOME}/.fonts
     mkdir -p ${HOME}/.fonts.conf.d/
-    ln -s ${PEARL_HOME}/packages/powerline/module/font/PowerlineSymbols.otf ${HOME}/.fonts
+    ln -s ${PEARL_PKGDIR}/module/font/PowerlineSymbols.otf ${HOME}/.fonts
     fc-cache -vf ${HOME}/.fonts
 
-    cp ${PEARL_HOME}/packages/powerline/module/font/10-powerline-symbols.conf ${HOME}/.fonts.conf.d/
+    cp ${PEARL_PKGDIR}/module/font/10-powerline-symbols.conf ${HOME}/.fonts.conf.d/
 
     info "Vim binding applied"
     if ask "Do you want Powerline binding also for Bash?" "N"
 
     then
-        apply "source ${PEARL_HOME}/packages/powerline/module/powerline/bindings/bash/powerline.sh" "${HOME}/.bashrc" false
+        apply "source ${PEARL_PKGDIR}/module/powerline/bindings/bash/powerline.sh" "${HOME}/.bashrc" false
     fi
     if ask "Do you want Powerline binding also for Tmux?" "N"
     then
-        apply "source ${PEARL_HOME}/packages/powerline/module/powerline/bindings/tmux/powerline.conf" "${HOME}/.tmux.conf" false
+        apply "source ${PEARL_PKGDIR}/module/powerline/bindings/tmux/powerline.conf" "${HOME}/.tmux.conf" false
     fi
 
     return 0
@@ -28,8 +28,8 @@ function pre_remove(){
     local powerline_conf_file="${HOME}/.fonts.conf.d/10-powerline-symbols.conf"
     [ -f "$powerline_conf_file" ] && rm -f $powerline_conf_file
 
-    unapply "source ${PEARL_HOME}/packages/powerline/module/powerline/bindings/bash/powerline.sh" "${HOME}/.bashrc"
-    unapply "source ${PEARL_HOME}/packages/powerline/module/powerline/bindings/tmux/powerline.conf" "${HOME}/.tmux.conf"
+    unapply "source ${PEARL_PKGDIR}/module/powerline/bindings/bash/powerline.sh" "${HOME}/.bashrc"
+    unapply "source ${PEARL_PKGDIR}/module/powerline/bindings/tmux/powerline.conf" "${HOME}/.tmux.conf"
 
     return 0
 }
